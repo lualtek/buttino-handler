458062fd3b1f74ade9a42381d0d677a8da4ca790
LasaleFamine <info@godev.space>
2024-02-23 12:55:03 +0100

    feat: added better sleeping

--- src/ButtinoRAK.cpp
+++ src/ButtinoRAK.cpp
@@ -1,6 +1,7 @@
 #include "ButtinoRAK.h"
 
 int counterClick = 0;
+bool isSleeping = false;
 unsigned long lastClickTime = 0;
 unsigned long startLongPressTime = 0;
 int __pin;
@@ -39,18 +40,12 @@ void wakeupCallback()
   {
     BUTTINOLOG(F("ButtinoRAK .wakeupCallback()"), "Clicked 5 times, shutting down...");
     counterClick = 0;
-    while (1)
-    {
-      api.system.sleep.setup(RUI_WAKEUP_FALLING_EDGE, __pin);
-      api.system.sleep.all(0); // Set to sleep forever.
-      pinMode(__pin, INPUT_PULLUP);
-      delay(BUTTINORAK_REBOOT_DELAY); // Long press to turn off/on, the long press time is 5s.
-      if (digitalRead(__pin) == LOW)
-      {
-        BUTTINOLOG(F("ButtinoRAK .wakeupCallback()"), "Long pressed, rebooting...");
-        api.system.reboot();
-      }
-    }
+    api.system.timer.stop(RAK_TIMER_0);
+    api.lorawan.deviceClass.set(RAK_LORA_CLASS_A);
+    isSleeping = true;
+    api.system.sleep.setup(RUI_WAKEUP_FALLING_EDGE, __pin);
+    api.system.sleep.all(0); // Set to sleep forever.
+    return;
   }
 
   lastClickTime = millis();
